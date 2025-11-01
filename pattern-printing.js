a3c9c3d7f77d950be7045e39d22e050d9ec8b970
Govind Kumar <govind.byte@gmail.com>
2025-11-01 17:44:33 +0530

    pattern pt1

---
+++ pattern-printing.js
@@ -0,0 +1,9 @@
+let prompt = require('prompt-sync')();
+let a = Number(prompt("Enter a number : "))
+// console.log(a);   // or process.stdout.write(a). it will not run in chrome
+for(let i = 1; i <= a; i++){
+    for(let j = 1; j<=a; j++){
+       process.stdout.write("* ");       // its printing in row, But if i use clog it will print in cloumn 
+    } 
+    console.log(); // Adding new lines 
+}
