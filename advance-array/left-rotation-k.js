1df6f63a8e89f5041820b1f69724f3b8e4f5d3f3
Govind Kumar <govind.byte@gmail.com>
2025-11-02 17:41:11 +0530

    newAP

--- advance-array/left-rotation-k.js
+++ advance-array/left-rotation-k.js
@@ -1,17 +1,32 @@
 // left rotation by K element
-let arr = [1, 2, 3, 4, 5];
+// This approach has time complexity n^2
+// let arr = [1, 2, 3, 4, 5];
+// let prompt = require("prompt-sync")();
+// let k = Number(prompt("Enter K value : "));
+
+// if (k < arr.length) {     // Handling Error if the input of k is greater than length of array
+//   for (j = 0; j < k; j++) {   // Nested loooping for Kth time of execution of left rotation
+//     let copy = arr[0];
+//     for (let i = 0; i < arr.length - 1; i++) {     // Loop for Left rotation
+//       arr[i] = arr[i + 1];
+//     }
+//     arr[arr.length - 1] = copy;
+//   }
+//   console.log(arr);
+// } else {
+//   console.log("The value of K is greater than length of array");
+// }
+
+
+// Another approach 
+// temp = arr[(i+k) % arr.length]   this is a efficient approach 
+
+let arr = [1,2,3,4,5];
+let temp = new Array(arr.length);
 let prompt = require("prompt-sync")();
 let k = Number(prompt("Enter K value : "));
-
-if (k < arr.length) {     // Handling Error if the input of k is greater than length of array
-  for (j = 0; j < k; j++) {   // Nested loooping for Kth time of execution of left rotation
-    let copy = arr[0];
-    for (let i = 0; i < arr.length - 1; i++) {     // Loop for Left rotation
-      arr[i] = arr[i + 1];
-    }
-    arr[arr.length - 1] = copy;
-  }
-  console.log(arr);
-} else {
-  console.log("The value of K is greater than length of array");
+k = k % arr.length;     // Error handling like previous where we used if else
+for (let i = 0; i<arr.length; i++){
+    temp[i] = arr[(k+i) % arr.length];
 }
+console.log(temp);
